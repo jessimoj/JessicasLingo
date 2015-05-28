f534d428abc9db4906061d5a4d222ba15e339484
Jessica Majlund <jessica.majlund@live.se>
2015-05-28 14:57:00 +0200

    Poängräknare samt felmeddelande tillagt

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 
 //Slumpar ett ord från listan
-var points = 0;
-var ordlista = ["syren", "trend", "storm"];
-var valtOrd = ordlista[Math.floor(Math.random()*3)];
+var points = 5000;
+var ordlista = ["syren", "trend", "storm", "julig", "kaffe"];
+var valtOrd = ordlista[Math.floor(Math.random()*5)];
 console.log(valtOrd);
 
 var spelrad = 0;
@@ -38,18 +38,35 @@ var spelplan = document.querySelectorAll("tr")[spelrad];
 	}
 
 
+	//OM SVARET ÄR AV FEL LÄNGD ELLER EJ BOKSTÄVER
+		if (anvandarOrd == "" || anvandarOrd.length < 5) {
+			alert("Din gissning måste innehålla minst 5 bokstäver.");
+
+		};
+
+
 	if (valtOrd === anvandarOrd) {
-		alert("Snyggt, du gissade rätt!");
+
+		alert("Snyggt, du gissade rätt! Du samlade ihop " + points + " poäng");
 
 		//rensa alla tds
-		
+		for(var i = 0; i < 25; i++) {
+			document.getElementsByTagName("td")[i].style.background = "#EED2EE";
+			document.getElementsByTagName("td")[i].innerHTML = "";
+		}
+
 	}
 
 	else {
-		
+			points -= 1000;
 			spelrad++;
+
+			if (points < 1000){
+				alert("Tyvärr, du förlorade. Det valda ordet var " + valtOrd + ".")
+			}
 		};
 	
+
 
 
 	//Rätt eller fel-meddelande
