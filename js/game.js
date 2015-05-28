fe508be71d125670f2704904b78a1d04779f9fbd
Jessica Majlund <jessica.majlund@live.se>
2015-05-28 13:46:06 +0200

    Gissningsrader aktiverade

--- js/game.js
+++ js/game.js
@@ -1,20 +1,31 @@
 
 //Slumpar ett ord från listan
+var points = 0;
 var ordlista = ["syren", "trend", "storm"];
 var valtOrd = ordlista[Math.floor(Math.random()*3)];
 console.log(valtOrd);
 
+var spelrad = 0;
+var spelplan = document.querySelectorAll("tr")[spelrad];
+
+
 //Hämtar spelarens gissning
-var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function(){
-	var anvandarOrd = document.getElementsByTagName("input")[0].value;
+
+	var button = document.getElementsByTagName("button")[0];
+	button.addEventListener("click", function(){
+		var anvandarOrd = document.getElementsByTagName("input")[0].value;
+
+
 
 //HÄMTA RUTORNA OCH LÄGG I RÄTT RUTOR
 	var rutor = document.getElementsByTagName("input")[0].value;
+	
 
-	var rutor = document.getElementsByTagName("td");
+//HÄMTA RAD PÅ SPELPLAN
+	var spelplan = document.querySelectorAll("tr")[spelrad];
+	var rutor = spelplan.getElementsByTagName("td");
 
-
+	//RÄTTNING AV GISSNING
 	for(var i = 0; i < 5; i++) {
 		rutor[i].innerHTML = anvandarOrd[i];
 
@@ -26,6 +37,22 @@ button.addEventListener("click", function(){
 			}
 	}
 
+
+	if (valtOrd === anvandarOrd) {
+		alert("Snyggt, du gissade rätt!");
+
+		//rensa alla tds
+		
+	}
+
+	else {
+		
+			spelrad++;
+		};
+	
+
+
+	//Rätt eller fel-meddelande
 	/*
 		if (valtOrd === anvandarOrd) {
 			alert("GRATTIIIIS!");
@@ -33,5 +60,9 @@ button.addEventListener("click", function(){
 		else{
 			alert("Försök igen, kompis!");
 		}
+		*/
+
 	});
-	*/
+	
+
+
